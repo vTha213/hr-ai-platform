d3e6e2e9b5f9967e3c4eb9a58105ea70ebbb88d9
Vishwanath Thakur <thakurvishwanath565@gmail.com>
2026-06-30 11:29:47 +0530

    Migrate to psycopg

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,5 +1,3 @@
-import ssl
-
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     AsyncSession,
@@ -8,28 +6,19 @@ from sqlalchemy.ext.asyncio import (
 
 from app.core.config import settings
 
-
-ssl_context = ssl.create_default_context()
-
 engine = create_async_engine(
     settings.DATABASE_URL,
     echo=False,
     pool_pre_ping=True,
-    connect_args={
-        "ssl": ssl_context
-    }
 )
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
-    expire_on_commit=False
+    expire_on_commit=False,
 )
 
 
 async def get_db():
     async with AsyncSessionLocal() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+        yield session
